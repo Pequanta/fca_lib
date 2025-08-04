3f5b8a12e406b4f1e6d2d8a929858c51e34ca072
Pequanta <Penielyohannes6@gmail.com>
2025-08-04 08:02:13 +0300

    fix: the returned value which represented the transpose of already obtained data is removed as its usage is become obsolet

--- fca/encoders.py
+++ fca/encoders.py
@@ -20,7 +20,7 @@ class Encoder:
             return [np.asarray([])]
 
 
-        attributes_ = np.asarray(data.columns[2:])
+        attributes_ = np.asarray(data.columns[1:])
 
         objects_ = data[data.columns[1]].to_numpy()
         data = data[attributes_]
@@ -28,8 +28,7 @@ class Encoder:
      
         relation_to_numpy = data.to_numpy()
         object_attribute_encoded = np.asarray([self.bitset_from_row(row) for row in relation_to_numpy])
-        attribute_object_encoded = np.asarray([self.bitset_from_row(row) for row in relation_to_numpy.T])
-        return [object_attribute_encoded, attribute_object_encoded, objects_, attributes_]
+        return [object_attribute_encoded, objects_, attributes_]
     
     def numpy_encoder(self, data) -> List[np.ndarray]:
         if type(data) != np.ndarray:
@@ -37,17 +36,16 @@ class Encoder:
             return [np.ndarray([])]
         attributes_ = data[0][2:]
         objects_ = np.asarray([data[i][1] for i in range(1, len(data))])
-        relation_encoded = np.asarray([self.bitset_from_row(row[1:]) for row in data[1:]])
-        inverse_relation_encoded = np.asarray([self.bitset_from_row(row[1:]) for row in data.T[2:]])
-        return [relation_encoded,inverse_relation_encoded , objects_, attributes_]
+        object_attribute_encoded = np.asarray([self.bitset_from_row(row[1:]) for row in data[1:]])
+        return [object_attribute_encoded, objects_, attributes_]
                    
     def bitset_from_row(self, row):
         result = (1 << len(row)) - 1
-        i = 0
-        while i < len(row):
+        i = len(row) - 1
+        while i > 0:
             if row[i] not in {'True', True, 'X', 'x', '1', 1}:
                 result ^= (1 << i)
-            i += 1
+            i -= 1
         return result
 
 
