6371728d4ea9319a51a29ef661a2f5c92b92d9d8
Pequanta <Penielyohannes6@gmail.com>
2025-08-31 11:11:39 +0300

    fix: some adjustements to align with dirac implementation. Also some of the unused parameters are removed from function arguments

--- scripts/examples/jsm_method/data_preprocessing.py
+++ scripts/examples/jsm_method/data_preprocessing.py
@@ -9,22 +9,19 @@ encoder = Encoder()
 
 
 class PreprocessingJSM:
-    def __init__(self, data_train: ndarray | pd.DataFrame, data_test: ndarray | pd.DataFrame, goal_train: List[bool], goal_test: List[bool]):
-        if type(data_train) == pd.DataFrame and type(data_test) == pd.DataFrame:
+    def __init__(self, data_train: ndarray | pd.DataFrame,  goal_train: List[bool]):
+        if type(data_train) == pd.DataFrame:
             ##Getting the target class
             self.goal_train = goal_train
-            self.goal_test = goal_test
 
             #The encoder class should only be used with the structural attributes. So the 
             #target class should be removed from the attributes list
 
             data_train.drop(columns=[data_train.columns[-1]], axis=1, inplace=True)
-            data_test.drop(columns=[data_test.columns[-1]], axis=1, inplace=True)
 
             self.object_attribute_encoded, self.attribute_object_encoded, self.objects, self.attributes = encoder.pandas_encoder(data_train) # type: ignore
-            self.negative_examples = data_train.loc[[i for i, val in data_train.iterrows() if self.goal_train[i] == False]]
-            self.positive_examples = data_train.loc[[i for i, val in data_train.iterrows() if self.goal_train[i] == True]]
-            self.unknown_examples = data_test.reset_index(drop=True)
+            self.negative_examples = data_train.loc[[i for i, val in data_train.iterrows() if self.goal_train[i] == False]] #type: ignore   
+            self.positive_examples = data_train.loc[[i for i, val in data_train.iterrows() if self.goal_train[i] == True]] #type: ignore
 
             self.size = len(data_train.columns) - 1
 
@@ -42,15 +39,19 @@ class PreprocessingJSM:
         """
         if type == 1:
              data_ = self.positive_examples
-        elif type == 0:
-             data_ = self.unknown_examples
         else:
              data_ = self.negative_examples
-        object_attribute_encoded,attribute_object_encoded,objects_,attributes_ = encoder.pandas_encoder(data_)
+        object_attribute_encoded,attribute_object_encoded,objects_,attributes_ = encoder.pandas_encoder(data_) #type: ignore
 
-        return object_attribute_encoded, attribute_object_encoded, objects_, attributes_    
+        return object_attribute_encoded, attribute_object_encoded, objects_, attributes_
 
-    def process_data(self) -> Tuple[List[int], List[int], List[int]]:
+    @staticmethod
+    def encode_test_data(data_test: pd.DataFrame):
+        data = data_test.reset_index(drop=True)
+        object_attribute_encoded, attribute_object_encoded, objects_, attributes_ = encoder.pandas_encoder(data) #type: ignore
+        return object_attribute_encoded, attribute_object_encoded, objects_, attributes_
+
+    def process_data(self) -> Tuple[List[int], List[int]]:
         """   
             Args:
                 data (List[int]): The input data to be processed.
@@ -60,8 +61,7 @@ class PreprocessingJSM:
         """
         pos_context , _ , _ , _= self.encode_data(1)
         negative_context, _ , _, _ = self.encode_data(-1)
-        undetermined_context, _, _, _ = self.encode_data(0)
-        return (list(pos_context), list(negative_context), list(undetermined_context))
+        return (list(pos_context), list(negative_context))
     def get_learning_data(self):
         """
             Create an instance of JSMMethodApplication with the provided contexts and objects/attributes.
@@ -73,7 +73,7 @@ class PreprocessingJSM:
         """
 
         learning_context = []
-        pos_context, negative_context, _  = self.process_data()
+        pos_context, negative_context = self.process_data()
         pos_context = list(map(lambda x: x ^ (1 << self.size), pos_context))
 
         learning_context.extend(pos_context)
